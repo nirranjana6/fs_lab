bfddd0015b719ff2ab83672d2d2d44679dbcdbd0
nirranjana6 <142658789+nirranjana6@users.noreply.github.com>
2024-11-22 12:30:25 +0530

    Update cicd_app.py

--- cicd_app.py
+++ cicd_app.py
@@ -1,8 +1,7 @@
- def add(a, b):
-       """Adds two numbers."""
-       return a + b
+def add(a, b):
+    """Adds two numbers."""
+    return a + b
 
-   def subtract(a, b):
-       """Subtracts two numbers."""
-       return a - b
-   
+def subtract(a, b):
+    """Subtracts two numbers."""
+    return a - b
